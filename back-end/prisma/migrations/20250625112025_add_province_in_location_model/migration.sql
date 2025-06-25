0072c5c1a5f49f767539548fa8d2ff5d8ec3722e
Phenphet <potpt1098@gmail.com>
2025-06-25 18:34:55 +0700

    create-location-model-and-controller

---
+++ back-end/prisma/migrations/20250625112025_add_province_in_location_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "location" ADD COLUMN     "location_province" TEXT;
---
+++ back-end/src/controller/locationController.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const app = express.Router()
+
+app.get('/location', async(req, res) => {
+    try {
+        const result = await prisma.location.findMany({
+            select: {
+                location_id: true,
+                location_name: true,
+            },
+            orderBy: {
+                location_id: 'desc'
+            }
+        })
+        
+        if (result.length > 0) {
+            res.status(200).send({
+                message: 'query data location success',
+                data: result
+            })
+        } else {
+            res.status(404).send({
+                message: 'somthing worng data not found',
+                data: []
+            })
+        }
+
+    } catch (error) {
+        res.status(500).send({error})
+    }
+})
+
+app.post('/location/create', async(req, res) => {
+    try {
+        const data = req.body
+        
+        const result = await prisma.location.create({data})
+        
+        if (result) {
+            res.status(200).send({
+                message: 'create data location success',
+                data: result
+            })
+        } else {
+            res.status(404).send({
+                message: 'somthing worng data not found',
+                data: []
+            })
+        }
+    } catch (error) {
+        res.status(500).send({error})
+    }
+}) 
+
+app.put('/location/update/:location_id', async(req, res) => {
+    try {
+
+        const location_id = req.params.location_id
+        const location_name = req.body.location_name
+        const location_province = req.body.location_province
+
+        const result = await prisma.location.update({
+            where: {
+                location_id: parseInt(location_id)
+            },
+            data: {
+                location_name: location_name,
+                location_province: location_province
+            }
+        })
+
+        if (result) {
+            res.status(200).send({
+                message: 'update data location success',
+                data: result
+            })
+        } else {
+            res.status(404).send({
+                message: 'somthing worng data not found',
+                data: []
+            })
+        }
+    } catch (error) {
+        res.status(500).send({error}) 
+    }
+})
+
+app.delete('/location/delete/:location_id', async(req, res) => {
+    try {
+        const location_id = req.params.location_id
+        const result = await prisma.location.delete({
+            where: {
+                location_id: parseInt(location_id)
+            }
+        })
+
+        if (result) {
+            res.status(200).send({
+                message: 'delete data location success',
+                data: result
+            })
+        } else {
+            res.status(404).send({
+                message: 'somthing worng data not found',
+                data: []
+            })
+        }
+    } catch (error) {
+        res.status(500).send({error})
+    }
+})
+
+module.exports = app
