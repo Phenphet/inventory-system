b27c6d74cb7bbb4afcffc404f386f9f3a2465a7b
Phenphet <potpt1098@gmail.com>
2025-06-30 14:18:25 +0700

    add component and include componect

---
+++ font-end/src/components/TableComponents.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+
+  import { onMounted, ref, watch } from 'vue'
+
+  interface TableInterface {
+    idTable: string
+    name: string
+    showData: any[]
+    columns: {data: string, title: string}[]
+    idField: string
+    handleEdit: (id: any) => void
+    handleDelete: (id: any) => void
+  }
+
+  const props = defineProps<TableInterface>()
+
+  const renderActionButtons = (rowId: string | number): string => {
+  return `
+    <button class="btn btn-sm btn-warning edit-btn" data-id="${rowId}">
+      <i class="fa-solid fa-pen-to-square"></i> Edit
+    </button>
+    <button class="btn btn-sm btn-danger delete-btn" data-id="${rowId}">
+      <i class="fa-solid fa-delete-left"></i> Delete
+    </button>
+  `
+}
+
+  onMounted(() => {
+    const dataTable = $(`#${props.idTable}`).DataTable({
+      "responsive": true,
+      "lengthChange": false,
+      "autoWidth": false,
+      "data": props.showData,
+      columns: [
+        ...props.columns,
+        {
+          data: null,
+          title: "",
+          orderable: false,
+          render: (data: any, type: string, row: any) => {
+            const id = row[props.idField]
+            return renderActionButtons(id)
+          }
+        }
+      ],
+      "buttons": [, "csv", "excel"]
+    })
+
+    $(`#${props.idTable} tbody`).on('click', '.edit-btn', function (this: HTMLElement) {
+      const id = $(this).data('id')
+      props.handleEdit(id)
+    })
+
+    $(`#${props.idTable} tbody`).on('click', '.delete-btn', function (this: HTMLElement) {
+      const id = $(this).data('id')
+      props.handleDelete(id)
+    })
+
+    watch(() => props.showData, (newData) => {
+      dataTable.clear().rows.add(newData).draw()
+    })
+  })
+</script>
+
+<template>
+ <table :id="props.idTable" class="table table-bordered table-striped "></table>
+</template>
--- font-end/src/views/ProductView.vue
+++ font-end/src/views/ProductView.vue
@@ -1,52 +1,37 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 import type { Product } from '@/controller/ProductController'
 import { productQuery } from '@/controller/ProductController'
 
-
+import ModalComponents from '@/components/ModalComponents.vue'
+import TableComponents from '@/components/TableComponents.vue'
 const showData = ref<Product[]>([])
+const showModal = ref<Boolean>(false)
+const isFocus = ref<any>(null)
+const formProduct = ref<Product>({
+  product_id: 0,
+  product_name: "",
+  product_reorder_level: 0,
+  product_unit: ""
+});
 
+
+const columns = [
+  {data: 'product_id', title: 'รหัสสินค้า'},
+  {data: 'product_name', title: 'ชื่อสินค้า'},
+  {data: 'product_unit', title: 'ประเภท'},
+]
 onMounted(async () => {
-
   showData.value = await productQuery()
-
-  const dataTable = $("#example1").DataTable({
-    "responsive": true,
-    "lengthChange": false,
-    "autoWidth": false,
-    "data": showData.value,
-     columns: [
-      { data: "product_name", title: "ชื่อสินค้า" },
-      { data: "product_reorder_level", title: "สินค้าต่ำกว่า" },
-      { data: "product_unit", title: "หน่วยเรียก" },
-      {
-        data: null,
-         title: "",
-        orderable: false,
-        render: (data: Product, type: string, row: Product) => {
-          return `
-            <button class="btn btn-sm btn-warning edit-btn" data-id="${row.product_id}"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
-            <button class="btn btn-sm btn-danger delete-btn" data-id="${row.product_id}"><i class="fa-solid fa-delete-left"></i> Delete</button>
-
-          `
-        }
-      }
-    ],
-    "buttons": [, "csv", "excel"]
-  })
-  dataTable.buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)')
-
-   $('#example1 tbody').on('click', '.edit-btn', function (this: HTMLElement) {
-    const id = $(this).data('id')
-    handleEdit(id)
-  })
-
-  $('#example1 tbody').on('click', '.delete-btn', function (this: HTMLElement) {
-    const id = $(this).data('id')
-    handleDelete(id)
-  })
 })
+
+const openModalfunc = () => {
+     showModal.value = !showModal.value
+  clearFrom()
+}
+const saveBtnFunc = () => {console.log('save data')}
+const closeModalFunc = () => { showModal.value = !showModal.value }
 
 const handleEdit = (id: Number) => {
   console.log(id)
@@ -55,19 +40,28 @@ const handleEdit = (id: Number) => {
 const handleDelete = (id: Number) => {
   console.log(id)
 }
+
+const clearFrom = () => {
+  formProduct.value.product_id = 0
+  formProduct.value.product_name = ''
+  formProduct.value.product_reorder_level = 0
+  formProduct.value.product_unit = ''
+
+  // isFocus.value.focus()
+}
 </script>
 
 <template>
-  <section class="content-header">
+  <section class="content-header" :class="showModal ? 'modal-open ' : ''">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
           <h1>Product</h1>
         </div>
         <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <button class="btn btn-success">เพิ่มรายการสินค้า</button>
-          </ol>
+          <div class="float-sm-right">
+            <button type="button" class="btn btn-success" @click="openModalfunc">เพิ่มรายการสินค้า</button>
+          </div>
         </div>
       </div>
     </div>
@@ -77,17 +71,26 @@ const handleDelete = (id: Number) => {
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-
           <div class="card">
             <div class="card-header">
               <h3 class="card-title">DataTable with default features</h3>
             </div>
             <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped "></table>
+              <TableComponents
+                :idTable="'showDataProduct'"
+                :idField="'product_id'"
+                :name="'product'"
+                :showData="showData"
+                :columns="columns"
+                :handleEdit="handleEdit"
+                :handleDelete="handleDelete"
+              ></TableComponents>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <ModalComponents :showModal="showModal" :closeBtn="closeModalFunc" :saveBtn="saveBtnFunc"></ModalComponents>
+  <div :class=" showModal ? 'modal-backdrop fade show' : ''" style="transition: 0s ease-in-out ;"></div>
 </template>
