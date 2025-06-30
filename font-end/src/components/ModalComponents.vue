57bbdc26790b76fb6cb72786604cd15bb218fb18
Phenphet <potpt1098@gmail.com>
2025-06-30 14:17:40 +0700

    change defin props

---
+++ font-end/src/components/ModalComponents.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+  interface ModalInterface {
+    titleModal?: String
+    showModal: Boolean
+    closeBtn: () => void
+    saveBtn: () => void
+  }
+
+  defineProps<ModalInterface>()
+
+</script>
+<template>
+    <div class="modal fade" :class="showModal ? 'show ' : ''" id="modal-lg" :aria-modal="showModal ? true : false" :style="showModal ? 'display: block; padding-right: 15px;': ''"
+    role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Large Modal</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeBtn">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <slot>
+
+          </slot>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal" @click="closeBtn">Close</button>
+          <button type="button" class="btn btn-primary" @click="saveBtn">Save changes</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+</template>
