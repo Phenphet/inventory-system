2e9d9b7e7c83713a2390346bf8e896c46f2a6c55
Phenphet <potpt1098@gmail.com>
2025-07-01 15:50:03 +0700

    add function save product

--- font-end/src/views/ProductView.vue
+++ font-end/src/views/ProductView.vue
@@ -6,16 +6,18 @@ import { productQuery } from '@/controller/ProductController'
 
 import ModalComponents from '@/components/ModalComponents.vue'
 import TableComponents from '@/components/TableComponents.vue'
+
 const showData = ref<Product[]>([])
 const showModal = ref<Boolean>(false)
-const isFocus = ref<any>(null)
+const isloadData = ref<Boolean>(true)
+const isStatusModal = ref<String>('Add')
+
 const formProduct = ref<Product>({
   product_id: 0,
   product_name: "",
   product_reorder_level: 0,
   product_unit: ""
 });
-
 
 const columns = [
   { data: 'product_name', title: 'ชื่อสินค้า' },
@@ -24,17 +26,42 @@ const columns = [
 ]
 
 onMounted(async () => {
+  setTimeout(() => {
+    isloadData.value = false
+  }, 2000)
+
   showData.value = await productQuery()
 })
 
 const openModalfunc = () => {
+  isStatusModal.value = 'Add'
   showModal.value = !showModal.value
   clearFrom()
 }
-const saveBtnFunc = () => { console.log('save data') }
+
+const saveBtnFunc = () => {
+  if (isStatusModal.value === 'Add') {
+    console.log('add-data')
+  } else {
+    console.log('edit-data')
+  }
+}
+
 const closeModalFunc = () => { showModal.value = !showModal.value }
 
 const handleEdit = (id: Number) => {
+
+  isStatusModal.value = 'Edit'
+
+  showModal.value = !showModal.value
+
+  const data = showData.value.find((items) => items.product_id === id)
+
+  formProduct.value.product_id = data!.product_id
+  formProduct.value.product_name = data!.product_name
+  formProduct.value.product_reorder_level = data!.product_reorder_level
+  formProduct.value.product_unit = data!.product_unit
+
   console.log(id)
 }
 
@@ -47,7 +74,6 @@ const clearFrom = () => {
   formProduct.value.product_name = ''
   formProduct.value.product_reorder_level = 0
   formProduct.value.product_unit = ''
-  // isFocus.value.focus()
 }
 </script>
 
@@ -67,7 +93,18 @@ const clearFrom = () => {
     </div>
   </section>
 
-  <div class="content">
+  <div class="content" v-if="isloadData">
+    <div class="container-fluid">
+      <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden"></span>
+          </div>
+          <p class="mt-3">&nbsp;&nbsp;&nbsp;Loading...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content" v-else>
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
@@ -84,31 +121,26 @@ const clearFrom = () => {
       </div>
     </div>
   </div>
+
   <ModalComponents :showModal="showModal" :closeBtn="closeModalFunc" :saveBtn="saveBtnFunc">
     <div class="card">
-        <div class="card-body">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputFile">File input</label>
-            <div class="input-group">
-              <div class="custom-file">
-                <input type="file" class="custom-file-input" id="exampleInputFile">
-                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-              </div>
-              <div class="input-group-append">
-                <span class="input-group-text">Upload</span>
-              </div>
-            </div>
-          </div>
+      <div class="card-body">
+        <div class="form-group">
+          <label>ชื่อสินค้า</label>
+          <input type="text" class="form-control" placeholder="ชื่อสินค้า" v-model="formProduct.product_name">
         </div>
+        <div class="form-group">
+          <label>จำนวนคงเหลือ</label>
+          <input type="number" class="form-control" placeholder="จำนวนคงเหลือ" v-model="formProduct.product_reorder_level">
+        </div>
+        <div class="form-group">
+          <label>หน่วยของสินค้า</label>
+          <input type="text" class="form-control" placeholder="หน่วย / ประเภท" v-model="formProduct.product_unit">
+        </div>
+      </div>
     </div>
   </ModalComponents>
+
   <div :class="showModal ? 'modal-backdrop fade show' : ''" style="transition: 0s ease-in-out ;"></div>
+
 </template>
