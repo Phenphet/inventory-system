bd6e9482c76ca098ed12afbc14d287ad6c5de9a4
Phenphet <potpt1098@gmail.com>
2025-07-04 18:30:29 +0700

    create stoce view IN|OUT|TRANFER

---
+++ font-end/src/views/StockViewIN.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import ModalComponents from '@/components/ModalComponents.vue'
+  import TableComponents from '@/components/TableComponents.vue'
+</script>
+
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      Stock In
+    </div>
+  </div>
+</template>
